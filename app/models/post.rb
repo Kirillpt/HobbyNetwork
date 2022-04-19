9dffcb0a7a6afc53f0911bf782857a4244c0adfe
kirp <kirp@dismail.de>
2022-04-19 16:56:28 +0300

    add validation to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,9 @@ class Post < ApplicationRecord
   belongs_to :category
   validates :title, presence: true
   validates :body, presence: true
+  validates :title, length: {maximum: 50}
   before_create { self.slug = SecureRandom.hex(6) }
+
 
   def self.search(search)
     where("body LIKE ?", "%#{search}%")
